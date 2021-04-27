a2e0420ef1b8e9d7f13dff6370cc37b09ba8ef58
zeebo26 <tanzibch@gmail.com>
2021-04-27 18:50:47 +0100

    Add Solution and Projects
    Adds Solution and WPF Project, Main view
    and Manual Sale View. Also Adds the
    ViewModels

---
+++ ZeeTechPOSLite/DesktopUI/ViewModels/MainViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace DesktopUI.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        #region Private Properties
+
+        private object _selectedViewModel;
+
+        #endregion
+
+        #region Construtors
+
+        public MainViewModel()
+        {
+            SelectedViewModel = new ManualSaleViewModel();
+        }
+        #endregion
+
+        #region Public Properties
+
+        public object SelectedViewModel
+        {
+            get { return _selectedViewModel; }
+            set
+            {
+                _selectedViewModel = value;
+                OnPropertyChanged(nameof(SelectedViewModel));
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+
+        #region INotifyPropertyChanged implimentation
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ ZeeTechPOSLite/DesktopUI/ViewModels/ManualSaleViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace DesktopUI.ViewModels
+{
+    class ManualSaleViewModel : INotifyPropertyChanged
+    {
+
+
+
+        #region INotifyPropertyChanged implimentation
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        #endregion
+    }
+}
