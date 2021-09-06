5cd9a5459e46e95185386170c67444f777a1a987
tanzib choudhury <tanzibch@gmail.com>
2021-09-07 00:42:54 +0100

    Add UpdateEditProductCommand

---
+++ ZeeTechPOSLite/DesktopUI/Commands/EditProductCommands/UpdateEditedProductCommand.cs
@@ -0,0 +1,27 @@
+﻿using DesktopUI.Services;
+using DesktopUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesktopUI.Commands.EditProductCommands
+{
+    public class UpdateEditedProductCommand : CommandBase
+    {
+        private readonly EditSaleProductViewModel _editSaleProductVM;
+        private readonly INavigationService _navigationService;
+
+        public UpdateEditedProductCommand(EditSaleProductViewModel editSaleProductVM, 
+            INavigationService navigationService)
+        {
+            _editSaleProductVM = editSaleProductVM;
+            _navigationService = navigationService;
+        }
+
+        public override void Execute(object parameter)
+        {
+            _editSaleProductVM.UpdateChanges();
+            _navigationService.Navigate();
+        }
+    }
+}
