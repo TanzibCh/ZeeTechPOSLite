d456ea7e30d46bb3c2426987f38b6aa13c0a8ff2
tanzib choudhury <tanzib_ch@hotmail.com>
2021-09-09 11:01:07 +0100

    Refund Command

---
+++ ZeeTechPOSLite/DesktopUI/Commands/EditSaleCommands/RefundCommand.cs
@@ -0,0 +1,24 @@
+﻿using DesktopUI.Services;
+using DesktopUI.Stores;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesktopUI.Commands.EditSaleCommands
+{
+    public class RefundCommand : CommandBase
+    {
+        private readonly INavigationService _navigationService;
+
+        public RefundCommand(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+        }
+
+        public override void Execute(object parameter)
+        {
+            // Call Refund Method
+            _navigationService.Navigate();
+        }
+    }
+}
