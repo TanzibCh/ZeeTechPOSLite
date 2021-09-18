77e318e45fa8ebe2ede9f0e8e820b4994f639d17
tanzib choudhury <tanzib_ch@hotmail.com>
2021-09-18 18:52:11 +0100

    Partial implimentation of ProductVM

--- ZeeTechPOSLite/DesktopUI/Models/ProductDisplayModel.cs
+++ ZeeTechPOSLite/DesktopUI/Models/ProductDisplayModel.cs
@@ -2,15 +2,17 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace DataAccessLibrary.Models
+namespace DesktopUI.Models
 {
-    public class ProductModel
+    public class ProductDisplayModel
     {
         public int Id { get; set; }
         public string ProductName { get; set; }
         public string ProductDescription { get; set; }
-        public int AverageCost { get; set; }
-        public int Price { get; set; }
+        public string Barcode { get; set; }
+        public decimal AverageCost { get; set; }
+        public decimal Price { get; set; }
         public string Department { get; set; }
+        public bool IsActive { get; set; }
     }
 }
---
+++ ZeeTechPOSLite/DesktopUI/ViewModels/ProductViewModel.cs
@@ -0,0 +1,93 @@
+﻿using DesktopUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace DesktopUI.ViewModels
+{
+    public class ProductViewModel : ViewModelBase
+    {
+        #region Field Properties
+
+        private ObservableCollection<ProductDisplayModel> _products;
+
+        // List to load the products
+        public ObservableCollection<ProductDisplayModel> Products
+        {
+            get { return _products; }
+            set
+            {
+                _products = value;
+                OnPropertyChanged(nameof(Products));
+            }
+        }
+
+        private string _Productsearch;
+
+        // Field for searching priducts
+        public string ProductSearch
+        {
+            get { return _Productsearch; }
+            set
+            {
+                _Productsearch = value;
+                OnPropertyChanged(nameof(ProductSearch));
+            }
+        }
+
+        private string _filterDepartment;
+
+        // Deparment filter
+        public string FilterDepartment
+        {
+            get { return _filterDepartment; }
+            set
+            {
+                _filterDepartment = value;
+                OnPropertyChanged(nameof(FilterDepartment));
+            }
+        }
+
+        private bool _sortCost;
+
+        // sort by cost
+        public bool SortCost
+        {
+            get { return _sortCost; }
+            set
+            {
+                _sortCost = value;
+                OnPropertyChanged(nameof(SortCost));
+            }
+        }
+
+        private bool _sortPrice;
+
+        // sort by price
+        public bool SortPrice
+        {
+            get { return _sortPrice; }
+            set
+            {
+                _sortPrice = value;
+                OnPropertyChanged(nameof(SortPrice));
+            }
+        }
+
+        private bool _sortName;
+
+        // sort by name
+        public bool SortName
+        {
+            get { return _sortName; }
+            set
+            {
+                _sortName = value;
+                OnPropertyChanged(nameof(SortName));
+            }
+        }
+
+        #endregion Field Properties
+    }
+}
