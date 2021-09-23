416558bd38f495e252e228de06eecb1c7c96c568
tanzib choudhury <tanzib_ch@hotmail.com>
2021-09-23 13:16:03 +0100

    Add CompleteReturnCommand

---
+++ ZeeTechPOSLite/DesktopUI/Commands/CompleteReturnCommands/CompleteReturnCommand.cs
@@ -0,0 +1,26 @@
+﻿using DesktopUI.Stores;
+using DesktopUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesktopUI.Commands.CompleteReturnCommands
+{
+    public class CompleteReturnCommand : CommandBase
+    {
+        private readonly ReturnCompleteViewModel _returnCompleteViewModel;
+        private readonly CreditStore _creditStore;
+
+        public CompleteReturnCommand(ReturnCompleteViewModel returnCompleteViewModel,
+            CreditStore creditStore)
+        {
+            _returnCompleteViewModel = returnCompleteViewModel;
+            _creditStore = creditStore;
+        }
+
+        public override void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
