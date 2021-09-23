cd4c0cdbada04c0d838d7915ec1bb9a89f6a0d65
tanzib choudhury <tanzib_ch@hotmail.com>
2021-09-23 13:20:45 +0100

    Add missing components from last upload

---
+++ ZeeTechPOSLite/DesktopUI/Commands/RefundCommands/CompleteCommand.cs
@@ -0,0 +1,31 @@
+﻿using DesktopUI.Services;
+using DesktopUI.Stores;
+using DesktopUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesktopUI.Commands.RefundCommands
+{
+    public class CompleteCommand : CommandBase
+    {
+        private readonly INavigationService _navigationService;
+        private readonly CreditStore _creditStore;
+        private readonly RefundViewModel _refundViewModel;
+
+        public CompleteCommand(INavigationService navigationService,
+            CreditStore creditStore, RefundViewModel refundViewModel)
+        {
+            _navigationService = navigationService;
+            _creditStore = creditStore;
+            _refundViewModel = refundViewModel;
+        }
+
+        public override void Execute(object parameter)
+        {
+            _creditStore.SaleId = _refundViewModel.SaleId;
+            _creditStore.Amount = _refundViewModel.RefundTotal;
+            _navigationService.Navigate();
+        }
+    }
+}
