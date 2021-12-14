e1916d04b0c1169c6d7eb17ec8932681a1d9ef83
tanzib choudhury <tanzib_ch@hotmail.com>
2021-12-14 20:25:29 +0000

    Add CreateProductCommand

---
+++ ZeeTechPOSLite/DesktopUI/Commands/ProductCommands/CreateProductCommand.cs
@@ -0,0 +1,24 @@
+﻿using DesktopUI.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopUI.Commands.ProductCommands
+{
+    internal class CreateProductCommand : CommandBase
+    {
+        private readonly INavigationService _navigationService;
+
+        public CreateProductCommand(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+        }
+
+        public override void Execute(object parameter)
+        {
+            _navigationService.Navigate();
+        }
+    }
+}
